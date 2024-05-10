7b4febe61b12d14b1347d5c6e1cbaf708e59fc23
riyanda432 <febry.priyangga@gmail.com>
2024-05-10 14:48:41 +0700

    test

---
+++ index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+
+app.get("/test", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
