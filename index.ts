b82a6f8035d1b5889bd71f8082c884cce81ccb9b
riyanda432 <febry.priyangga@gmail.com>
2024-05-10 18:40:29 +0700

    test folder destination

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/test", (_req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel"));
+app.get("/test", (_req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel Test Change"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
